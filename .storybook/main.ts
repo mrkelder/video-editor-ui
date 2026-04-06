df6c04e4748a6960cfd88bca4f0ab289cbbbd9be
Vladyslav Aheiev <vageev06@gmail.com>
2026-04-06 16:30:49 -0400

    fix: disable storybook telemetry

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,5 +3,8 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.story.tsx"],
   framework: "@storybook/react-vite",
+  core: {
+    disableTelemetry: true,
+  },
 };
 export default config;
