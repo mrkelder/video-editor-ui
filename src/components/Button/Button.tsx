7b7ccfefbcc47419aa50141655cbe0710c1fe466
Egor <ururugeorge@gmail.com>
2026-04-07 21:06:53 +0300

    feat: add onclick support to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,6 +5,7 @@ interface IButton {
 	children: string
 	type?: ButtonType
 	disabled?: boolean
+	onClick?: () => void
 	classname?: string
 }
 
@@ -12,12 +13,14 @@ export function Button({
 	children,
 	type = 'button',
 	disabled = false,
+	onClick,
 	classname,
 }: IButton) {
 	return (
 		<button
 			type={type}
 			disabled={disabled}
+			onClick={onClick}
 			className={`px-4 py-2 rounded-md font-medium ${disabled ? 'bg-gray-400 text-gray-200 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 text-white shadow'} ${classname}`}
 		>
 			{children}
