aa00a49ac30145821916adfbb81095f34cca4de3
Egor <ururugeorge@gmail.com>
2026-04-07 20:49:20 +0300

    fix: incorrect tailwind class in button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -18,7 +18,7 @@ export function Button({
 		<button
 			type={type}
 			disabled={disabled}
-			className={`px-4 py-2 rounded-md font-medium ${disabled ? 'bg-gray-400 text-gray-200 cursor-now-allowed' : 'bg-blue-500 hover:bg-blue-600 text-white shadow'} ${classname}`}
+			className={`px-4 py-2 rounded-md font-medium ${disabled ? 'bg-gray-400 text-gray-200 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 text-white shadow'} ${classname}`}
 		>
 			{children}
 		</button>
