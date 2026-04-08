d5b94c903badadf2b89aa75bf97d1b342a8f81bd
Egor <ururugeorge@gmail.com>
2026-04-08 20:52:42 +0300

    feat: move Button styles to variables

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,12 +6,17 @@ export function Button({
 	disabled = false,
 	onClick,
 }: ButtonProps) {
+	const disabledButtonStyle = 'bg-gray-400 text-gray-200 cursor-not-allowed'
+	const enabledButtonStyle = 'bg-blue-500 hover:bg-blue-600 text-white shadow'
+
+	const totalClassName = `px-4 py-2 rounded-md font-medium transition-colors ${disabled ? disabledButtonStyle : enabledButtonStyle}`
+
 	return (
 		<button
 			type={type}
 			disabled={disabled}
 			onClick={onClick}
-			className={`px-4 py-2 rounded-md font-medium ${disabled ? 'bg-gray-400 text-gray-200 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600 text-white shadow'}`}
+			className={totalClassName}
 		>
 			{children}
 		</button>
