68f9287d2b65cb5983327500ada5bf37390f611e
Egor <ururugeorge@gmail.com>
2026-04-13 21:47:22 +0300

    fix: change classname to className in InputProps

--- src/components/Input/Input.types.ts
+++ src/components/Input/Input.types.ts
@@ -2,7 +2,7 @@ import type { InputHTMLAttributes } from 'react'
 
 export interface InputProps extends Omit<
 	InputHTMLAttributes<HTMLInputElement>,
-	'classname'
+	'className'
 > {
 	label: string
 	name: string
