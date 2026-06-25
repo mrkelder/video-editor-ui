c9f4a48671adf6c5f560a9b7c58aadbde52f56d6
Egor <ururugeorge@gmail.com>
2026-06-25 18:49:39 +0300

    feat: use icon component in password input

--- src/components/PasswordInput/PasswordInput.tsx
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -1,24 +1,28 @@
 import { useState } from 'react'
 import { Input } from '../Input'
+import { Icon } from '../Icon'
 import type { PasswordInputProps } from './PasswordInput.types'
 
 export function PasswordInput(props: PasswordInputProps) {
 	const [isPasswordVisible, setIsPasswordVisible] = useState(false)
 
-	return <Input 
-						{...props} 
-						type={isPasswordVisible ? 'text' : 'password'} 
-						rightElement={
-							<button
-								type="button"
-								onClick={() => setIsPasswordVisible(prev => !prev)}
-								className='text-gray-400 hover:text-gray-600'
-								aria-label={
-									isPasswordVisible ? 'Hide password' : 'Show password'
-								}
-							>
-								{isPasswordVisible ? 'Hide' : 'Show'}
-							</button>
-						} 
+	return (
+		<Input
+			{...props}
+			type={isPasswordVisible ? 'text' : 'password'}
+			rightElement={
+				<button
+					type='button'
+					onClick={() => setIsPasswordVisible(prev => !prev)}
+					className='text-gray-400 hover:text-gray-600'
+					aria-label={isPasswordVisible ? 'Hide password' : 'Show password'}
+				>
+					<Icon
+						name={isPasswordVisible ? 'visibility_off' : 'visibility'}
+						size={20}
 					/>
+				</button>
+			}
+		/>
+	)
 }
