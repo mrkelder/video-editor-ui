054488a23ea58bf35ef01b3c08b4174f300e5567
Egor <ururugeorge@gmail.com>
2026-06-25 17:47:05 +0300

    feat: refactor restrict password input props

--- src/components/PasswordInput/PasswordInput.types.ts
+++ src/components/PasswordInput/PasswordInput.types.ts
@@ -1,3 +1,3 @@
 import type { InputProps } from '@components/Input'
 
-export type PasswordInputProps = Omit<InputProps, 'type'>
+export type PasswordInputProps = Omit<InputProps, 'type' | 'rightElement'>
