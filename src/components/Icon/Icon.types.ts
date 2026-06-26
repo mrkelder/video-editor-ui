ce7a78b29689267e4af3e1e5a783d1d3b76c0f82
Egor <ururugeorge@gmail.com>
2026-06-26 11:09:42 +0300

    feat: refactor restrict icon props

--- src/components/Icon/Icon.types.ts
+++ src/components/Icon/Icon.types.ts
@@ -1,6 +1,17 @@
 export interface IconProps {
 	name: string
 	size?: number
-	className?: string
 	ariaHidden?: boolean
 }
+
+export type IconName = 
+	| 'visibility'
+	| 'visibility_off'
+	| 'search'
+	| 'add'
+	| 'calendar_month'
+	| 'check'
+	| 'close'
+	| 'edit'
+	| 'delete'
+	| 'attach_file'
