45c05ee83dc072abda4c4760135302d7fd0de43b
Egor <ururugeorge@gmail.com>
2026-08-05 12:25:08 +0300

    feat: add login page route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import { HomePage } from "@pages/HomePage";
-import { NotFoundPage } from "@pages/NotFoundPage";
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { HomePage } from '@pages/HomePage'
+import { NotFoundPage } from '@pages/NotFoundPage'
+import { LoginPage } from '@pages/LoginPage'
 
 export function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
