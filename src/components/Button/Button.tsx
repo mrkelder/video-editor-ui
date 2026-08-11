83af643471affb0c143a347ea47f9411285a7d2f
Egor <ururugeorge@gmail.com>
2026-08-11 14:47:13 +0300

    fix: add pointer cursor to enabled button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ export function Button({
 	onClick,
 }: ButtonProps) {
 	const disabledButtonStyle = 'bg-gray-400 text-gray-200 cursor-not-allowed'
-	const enabledButtonStyle = 'bg-blue-500 hover:bg-blue-600 text-white shadow'
+	const enabledButtonStyle = 'bg-blue-500 hover:bg-blue-600 text-white shadow cursor-pointer'
 
 	const totalClassName = `px-4 py-2 rounded-md font-medium transition-colors ${disabled ? disabledButtonStyle : enabledButtonStyle}`
 
