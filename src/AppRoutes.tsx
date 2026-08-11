78ef73edcaa1249a5d104e568ba02a1c1a52e35d
Vladyslav Aheiev <vageev06@gmail.com>
2026-08-10 23:32:07 -0400

    fix: add missing login page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,9 +1,9 @@
-
 import { BrowserRouter, Route, Routes } from "react-router";
 import { HomePage } from "@pages/HomePage";
 import { NotFoundPage } from "@pages/NotFoundPage";
-import { SignUpPage } from '@pages/SignUpPage'
-import { PasswordRecoveryPage } from '@pages/PasswordRecoveryPage'
+import { SignUpPage } from "@pages/SignUpPage";
+import { PasswordRecoveryPage } from "@pages/PasswordRecoveryPage";
+import { LoginPage } from "@pages/LoginPage";
 
 export function AppRoutes() {
   return (
@@ -11,6 +11,7 @@ export function AppRoutes() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/forget-password" element={<PasswordRecoveryPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
